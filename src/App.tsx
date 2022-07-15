c2f430fd2fad77e469f58db57be0d2034a7a73ae
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-07-15 14:28:41 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 function App() {
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./styles.css";
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>      
+    <Router basename={process.env.PUBLIC_URL}>  
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
 
