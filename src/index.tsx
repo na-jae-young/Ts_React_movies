5e3eeccf20d230f6143f01060e7c0d015b246922
DESKTOP-ULAPNNB\hr <tprtnlzkwlsh@naver.com>
2022-07-15 13:58:28 +0900

    HashRouter 로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./styles.css";
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>      
+    <HashRouter basename={process.env.PUBLIC_URL}>      
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
